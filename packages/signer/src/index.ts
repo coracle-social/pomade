28a1648c9ba60a047afae63800a24e47e4aab29f
Jon Staab <shtaab@gmail.com>
2026-02-20 14:22:09 -0800

    Add cors headers

--- packages/signer/src/index.ts
+++ packages/signer/src/index.ts
@@ -56,6 +56,16 @@ const service = new Signer({
 })
 
 const server = http.createServer(async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS")
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+  if (req.method === "OPTIONS") {
+    res.writeHead(204)
+    res.end()
+    return
+  }
+
   if (req.method !== "POST") {
     res.writeHead(405, {"Content-Type": "application/json"})
     res.end(JSON.stringify({ok: false, message: "Method not allowed"}))
